c187bacb71d37b0f7b228ae3ef9a9cf814908b85
ariel <arielgordon123@gmail.com>
2020-02-11 17:42:36 +0200

    add clear function

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+from os import system, name
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 
 def get_choise(min=1, max=3):
