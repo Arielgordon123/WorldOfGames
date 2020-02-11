93afe8a5c656e252054d42f0cf6809d8b630627a
ariel <arielgordon123@gmail.com>
2020-02-11 19:12:40 +0200

    Load games

--- Live.py
+++ Live.py
@@ -1,15 +1,26 @@
+from GuessGame import GuessGame
+from CurrencyRouletteGame import CurrencyRouletteGame
+from MemoryGame import MemoryGame
 from utils import get_choise
+
 
 def welcome(name):
     return "Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play.".format(name=name)
 
 
 def load_game():
-    game_options = ["1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back",
-                    "2. Guess Game - guess a number and see if you chose like the computer",
-                    "3. Currency Roulette - try and guess the value of a random amount of USD in ILS"]
-    for game in game_options:
-        print(game)
-    choise = get_choise(1, len(game_options))
-    print("Please choose game difficulty from 1 to 5:")
-    difficulty = get_choise(1, 5)
+    games = [MemoryGame, GuessGame, CurrencyRouletteGame]
+    for i, game in enumerate(games):
+        print("{num}: {description}".format(
+            num=i+1, description=game.get_description()))
+    choise = get_choise(1, len(games))
+    difficulty = get_choise(
+        min=1, max=5, message="Please choose game difficulty from 1 to 5: ")
+    result = games[choise-1](difficulty).play()
+    if result:
+        print("you Won!")
+    else:
+        print("you lose, try again")
+
+
+load_game()
