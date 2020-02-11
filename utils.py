3fd06f5963ecfa03630de9f92c3dcc4f6eb2169d
ariel <arielgordon123@gmail.com>
2020-02-11 19:14:26 +0200

    add message parm to get_choise

--- utils.py
+++ utils.py
@@ -10,9 +10,9 @@ def clear():
         system('clear')
 
 
-def get_choise(min=1, max=3):
+def get_choise(min=1, max=3, message="Please enter your choise: "):
     try:
-        choise = int(input("Please enter your choise: "))
+        choise = int(input(message))
         if choise > max or choise < min:
             raise ValueError
         return choise
